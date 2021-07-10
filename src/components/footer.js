6291aa75c837a0ca7e61c883692fe891d5385ee6
pybala <youcompleteit@gmail.com>
2021-07-10 15:30:57 +0000

    social links

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,8 +5,8 @@ const Footer = () => {
   return (
     <footer className="bk-footer">
         &copy; {new Date().getFullYear()} <Link to="/">Balakumar</Link>
-        <span className="footer-link">by 
-          <a target="_blank" rel="noreferrer" href="https://www.netlify.com/">Netlify</a>, 
+        <span className="footer-link">by
+          <a target="_blank" rel="noreferrer" href="https://www.netlify.com/">Netlify</a>,
           <a target="_blank" rel="noreferrer" href="https://www.gatsbyjs.com">Gatsby</a>, React
         </span>
     </footer>
--- src/components/header.js
+++ src/components/header.js
@@ -16,9 +16,9 @@ const Header = ({ location, title, children }) => {
                 {header}
             </div>
             <div class="social-cont">
-                <a href="//github.com/kumarbalap" className="bkSocialIcon bkGithub" title="GitHub" target="_blank" rel="noreferrer">&nbsp;</a>
+                <a href="//github.com/pybala" className="bk-social-icon github-icon" title="GitHub" target="_blank" rel="noreferrer">&nbsp;</a>
                 <span></span>
-                <a href="//in.linkedin.com/in/kumarbalap" className="bkSocialIcon bkLinkedin" title="LinkedIn" target="_blank" rel="noreferrer">&nbsp;</a>
+                <a href="//in.linkedin.com/in/kumarbalap" className="bk-social-icon linkedin-icon" title="LinkedIn" target="_blank" rel="noreferrer">&nbsp;</a>
             </div>            
             <nav className="left-nav">
                 <Link to="/">Home</Link>
